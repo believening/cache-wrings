f0759f1bda084f7c070c22d69bae1bb2406be84e
wangkang <wangkang101@huawei.com>
2019-07-26 10:16:42 +0800

    finish cache go test

--- cache/inmemorycache_test.go
+++ cache/inmemorycache_test.go
@@ -16,7 +16,12 @@ func Test_inMemoryCache_Set(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			"set1",
+			&inMemoryCache{m: make(map[string][]byte), Stat: Stat{0, 0, 0}},
+			args{"set1", []byte{'s', 'e', 't', '1'}},
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -31,6 +36,7 @@ func Test_inMemoryCache_Get(t *testing.T) {
 	type args struct {
 		k string
 	}
+
 	tests := []struct {
 		name    string
 		imc     *inMemoryCache
@@ -38,7 +44,34 @@ func Test_inMemoryCache_Get(t *testing.T) {
 		want    []byte
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "GetNormal",
+			imc: &inMemoryCache{
+				m: func(k string, v []byte) map[string][]byte {
+					b := make(map[string][]byte)
+					b[k] = v
+					return b
+				}("GetNormal", []byte("OK")),
+				Stat: Stat{1, 9, 2},
+			},
+			args:    args{"GetNormal"},
+			want:    []byte("OK"),
+			wantErr: false,
+		},
+		{
+			name: "NotExist",
+			imc: &inMemoryCache{
+				m: func(k string, v []byte) map[string][]byte {
+					b := make(map[string][]byte)
+					b[k] = v
+					return b
+				}("testKey", []byte("OK")),
+				Stat: Stat{1, 7, 2},
+			},
+			args:    args{"NotExist"},
+			want:    nil,
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -64,7 +97,19 @@ func Test_inMemoryCache_Del(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Delete",
+			imc: &inMemoryCache{
+				m: func(k string, v []byte) map[string][]byte {
+					b := make(map[string][]byte)
+					b[k] = v
+					return b
+				}("DelTest", []byte("OK")),
+				Stat: Stat{1, 7, 2},
+			},
+			args:    args{"DetTest"},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -81,7 +126,18 @@ func Test_inMemoryCache_GetStat(t *testing.T) {
 		imc  *inMemoryCache
 		want Stat
 	}{
-		// TODO: Add test cases.
+		{
+			name: "getStat",
+			imc: &inMemoryCache{
+				m: func(k string, v []byte) map[string][]byte {
+					b := make(map[string][]byte)
+					b[k] = v
+					return b
+				}("stattest", []byte("OK")),
+				Stat: Stat{1, 8, 2},
+			},
+			want: Stat{1, 8, 2},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -97,7 +153,13 @@ func TestNewInMemCache(t *testing.T) {
 		name string
 		want Cache
 	}{
-		// TODO: Add test cases.
+		{
+			name: "TestNew",
+			want: &inMemoryCache{
+				m:    make(map[string][]byte),
+				Stat: Stat{0, 0, 0},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
--- cache/stat_test.go
+++ cache/stat_test.go
@@ -12,7 +12,8 @@ func TestStat_add(t *testing.T) {
 		s    *Stat
 		args args
 	}{
-		// TODO: Add test cases.
+		{"add1", &Stat{10, 100, 1000}, args{"add1", []byte{'a', 'b', 'c'}}},
+		{"add2",&Stat{1,4,4},args{"add1", []byte{'a', 'b', 'c','d'}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -31,7 +32,8 @@ func TestStat_del(t *testing.T) {
 		s    *Stat
 		args args
 	}{
-		// TODO: Add test cases.
+		{"add1", &Stat{10, 100, 1000}, args{"add1", []byte{'a', 'b', 'c'}}},
+		{"add2",&Stat{1,4,4},args{"add1", []byte{'a', 'b', 'c','d'}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
